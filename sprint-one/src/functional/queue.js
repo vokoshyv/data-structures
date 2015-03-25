def9a8db5c93041e81a199e33acd29bbe531b23b
vokoshyv <ronsuij@gmail.com>
2015-03-25 16:42:14 -0700

    Start on functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
+  var first = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+    if(size > 0){
+      var result = storage[first];
+      size--;
+      first++;
+      return result;
+    }
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.sizeVar = 0;
 };
 
+Stack.prototype.push = function(value){
+  this.storage[this.sizeVar] = value;
+  this.sizeVar++;
 
+
+};
+
+Stack.prototype.pop = function(){
+  if(this.sizeVar > 0){
+    var result = this.storage[this.sizeVar -1];
+    delete this.storage[this.sizeVar - 1];
+    this.sizeVar--;
+    return result;
+  }
+
+};
+
+Stack.prototype.size = function(){
+  return this.sizeVar;
+
+};
+
+var funStack = new Stack();
+console.log(funStack);
+console.log(funStack.sizeVar);

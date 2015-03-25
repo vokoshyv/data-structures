b0ddcc14edb0b717965994c59db559f563cfc6a2
vokoshyv <ronsuij@gmail.com>
2015-03-25 15:56:28 -0700

    implements protoypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.sizeVar = 0;
+
+  return someInstance;
 };
 
-var stackMethods = {};
 
+var stackMethods = {
+  push: function(value){
+    this.storage[this.sizeVar] = value;
+    this.sizeVar++;
+
+  },
+
+
+
+  pop: function(){
+    if(this.sizeVar > 0){
+      var result = this.storage[this.sizeVar -1];
+      delete this.storage[this.sizeVar -1];
+      this.sizeVar--;
+      return result;
+    }
+
+  },
+
+
+  size: function(){
+    return this.sizeVar;
+  }
+};
 

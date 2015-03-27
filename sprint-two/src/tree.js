90f841727840b054e88e3e4df4931ae50834fd3b
vokoshyv <ronsuij@gmail.com>
2015-03-26 20:21:16 -0700

    Begin tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,11 +16,32 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  // this.children[0] = {};
+//   this.children[0].value = value;
+//   this.children[0].children = [];
+//
+ var newChild = Node(value);
+//get newChild into this.children.
+//get lower level children into intermediate children
+  this.children.push(newChild);
+
 
 };
 
 treeMethods.contains = function(target){
 
+};
+
+
+var Node = function(value){
+  var node = {};
+
+  node.value = value;
+  node.children = [];
+
+  _.extend(node, treeMethods);
+
+  return node;
 };
 
 
